2e167ca51f3bebcfd1e468ac62e4829fa613619d
João Goulart <joao123goulart@gmail.com>
2022-09-20 01:28:48 -0300

    block2 add

---
+++ Tetris/Tetris/Block2.h
@@ -0,0 +1,48 @@
+/**********************************************************************************
+// Food (Arquivo de Cabeçalho)
+//
+// Criação:     03 Jan 2013
+// Atualização: 25 Ago 2021
+// Compilador:  Visual C++ 2019
+//
+// Descrição:   Comida do PacMan
+//
+**********************************************************************************/
+
+#ifndef _BLOCK2_H_
+#define _BLOCK2_H_
+
+// ---------------------------------------------------------------------------------
+// Inclusões
+
+#include "Types.h"                      // tipos específicos da engine
+#include "Object.h"                     // interface de Object
+#include "Sprite.h"                     // interface de Sprites
+#include "Block.h"                     // interface de Sprites
+
+
+// ---------------------------------------------------------------------------------
+
+class Block2 : public Object
+{
+private:
+    Sprite* sprite = nullptr;          // sprite da comida
+
+public:
+    Block2(Block * blocks[4]);                             // construtor
+    ~Block2();                            // destrutor
+
+    void Update();                      // atualização do objeto
+    void Draw();                        // desenho do objeto
+};
+
+// ---------------------------------------------------------------------------------
+
+inline void Block2::Draw()
+{
+    sprite->Draw(x, y, z);
+}
+
+// ---------------------------------------------------------------------------------
+
+#endif
