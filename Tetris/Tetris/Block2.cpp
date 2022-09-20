983c6cb487ecfb4f51c34b9e980d812770a7bcff
João Goulart <joao123goulart@gmail.com>
2022-09-20 03:11:26 -0300

    um commit ai

--- Tetris/Tetris/Block2.cpp
+++ Tetris/Tetris/Block2.cpp
@@ -13,8 +13,23 @@
 // Inclusões
 #include "Block2.h"
 
-Block2::Block2(Block * blocks[4]) {
-	
+Block2::Block2(Block * blocks[4], int blockType) {
+	switch (blockType) {
+	case BLOCKTYPE1:
+		sprite = new Sprite()
+		break;
+	case BLOCKTYPE2:
+		break;
+	case BLOCKTYPE3:
+		break;
+	case BLOCKTYPE4:
+		break;
+	case BLOCKTYPE5:
+		break;
+	case BLOCKTYPE6:
+		break;
+
+	}
 }
 
 Block2::~Block2() {
