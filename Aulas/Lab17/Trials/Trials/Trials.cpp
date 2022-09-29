73e2cee961b05ac9d034ac96c1abc678bad647dd
Leyvino Bezerra <47372809+LeyvinoBezerra@users.noreply.github.com>
2022-09-29 17:02:07 -0300

    29/09 -> Aula --> Ok!

--- Aulas/Lab17/Trials/Trials/Trials.cpp
+++ Aulas/Lab17/Trials/Trials/Trials.cpp
@@ -167,7 +167,7 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 
     // configura motor
     engine->window->Mode(WINDOWED);
-    engine->window->Size(800, 600);
+    engine->window->Size(1920, 1080);
     engine->window->Color(0, 0, 0);
     engine->window->Title("Trials");
     engine->window->Icon(IDI_ICON);
